c64f742adc2d5dc1232905294bd5c2dc6b53a57f
lihaybs <lihaybs94@gmail.com>
2022-03-27 03:43:11 +0300

    Restor tfvars file

---
+++ .history/DataServerRunUp_20220327034133.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+apt-get update
+
+
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+
+echo "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgdg main" | sudo tee /etc/apt/sources.list.d/pgdg.list
+
+apt-get update
+
+apt -y install postgresql-12 postgresql-client-12
+
+sed -ie "/^#listen_addresses/ a listen_addresses = '*'" /etc/postgresql/12/main/postgresql.conf
+
+bash -c "echo host    all          all            0.0.0.0/0  trust >> /etc/postgresql/12/main/pg_hba.conf"
+
+systemctl restart postgresql
+
+#_______________________________________#
+# Anoter solotion
+#_______________________________________#
+#!/bin/bash
+
+# apt-get update
+
+# apt install docker.io -y
+
+# docker pull postgres:latest
+
+# docker run --restart always -d --name measurements -p 5432:5432 -e 'POSTGRES_PASSWORD=Hakolzorem2022' postgres
---
+++ .history/RunUp_20220327031743.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+apt-get update
+
+git clone https://github.com/lihaybs/bootcamp-app.git
+
+cd /bootcamp-app
+
+curl -sL https://deb.nodesource.com/setup_14.x | sudo bash - #downloading nodejs libaries
+
+DEBIAN_FRONTEND=noninteractive apt-get install -y postgresql-client nodejs
+
+npm install dotenv
+
+npm install pm2@latest -g # insatalling pm2 for the app to run automaticlly after machine reboot
+
+cat <<EOF >.env
+# Host configuration
+PORT=8080
+HOST=0.0.0.0
+
+
+HOST_URL=http://20.121.73.232:8080
+COOKIE_ENCRYPT_PWD=superAwesomePasswordStringThatIsAtLeast32CharactersLong!
+NODE_ENV=development
+
+# Okta configuration
+OKTA_ORG_URL=https://dev-50792663.okta.com
+OKTA_CLIENT_ID=0oa3ytguv8aug1PTu5d7
+OKTA_CLIENT_SECRET=XRTjnYOnUO-gvWTnIcTjtyBGxCFjWIokmZIoOYhx
+
+# Postgres
+PGHOST=10.30.2.4
+PGUSERNAME=postgres
+PGDATABASE=postgres
+PGPASSWORD=Hakolzorem2022
+PGPORT=5432
+EOF
+
+npm run initdb #initializing the data base
+
+pm2 start "npm run dev" #starting the app and saving it to pm2 watch list
+
+pm2 save # saving pm2 watch list **important
